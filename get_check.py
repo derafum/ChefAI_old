0f37bdfa13dadce0eff780bf6c2204c57e439cf4
nodax-hub <ioann.ishenko.2003@gmail.com>
2023-03-08 00:53:53 +0400

    *переместил комментарии в docstring к методам класса; *добавил поле последнего ответа; *изменил метод сохранения в json; *перенёс получение переменных среды в main файл.

--- get_check.py
+++ get_check.py
@@ -1,30 +1,23 @@
-"""import requests
-import json
-url = 'https://proverkacheka.com/api/v1/check/get'
-data={'token':'20253.Umk1V1xWs9M87DoWY','qrraw': 't=20230306T1324&s=59.98&fn=7281440500232639&i=8355&fp=3680124697&n=1'}
-resp = requests.post(url, data=data)
-print(resp.text)"""
-import requests
-import json
 import os
-# создать модуль сканирования в основной ветке
-from dotenv import load_dotenv
 
-load_dotenv()
+import json
+import requests
 
 
 class ScanCheck:
     def __init__(self):
         self.url = 'https://proverkacheka.com/api/v1/check/get'
         self.token = os.getenv('token')
-    # получаем в str данные чека и переводим в json
+        self.last_response = None
+
     def get_json(self, qrraw: str):
+        """Получить результат проверки чека в json формате"""
         data = {'token': self.token,
                 'qrraw': qrraw}
-        resp = requests.post(self.url, data=data)
-        file_json = json.loads(resp.text)
-        return file_json
-    # сохраняем данные чека в виде json
-    def save_check_to_json(self, file_json):
-        with open('info_check.json', 'w') as f:
-            json.dump(file_json, f)
+        self.last_response = requests.post(self.url, data=data)
+        return json.loads(self.last_response.text)
+
+    def save_check_to_json(self, filename: str = 'info_check.json'):
+        """Сохраняем данные чека в виде json"""
+        with open(filename, mode='w', encoding='utf-8') as file:
+            json.dump(json.loads(self.last_response.text), file)
