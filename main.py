cb7d574ce5102fd3cb26b1af2ed56aaae6f40d8c
nodax-hub <ioann.ishenko.2003@gmail.com>
2023-03-08 01:08:56 +0400

    исправил ошибку

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     qrraw = 't=20230306T1324&s=59.98&fn=7281440500232639&i=8355&fp=3680124697&n=1'
     client = ScanCheck()
     check_json = client.get_json(qrraw)
-    client.save_check_to_json(check_json)
+    client.save_check_to_json()
