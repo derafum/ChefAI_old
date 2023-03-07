d28d42bcb01cde57823296682d32c7f6db286c53
nodax-hub <ioann.ishenko.2003@gmail.com>
2023-03-08 00:54:31 +0400

    Добавил получение переменных среды (виртуального окружения)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from get_check import ScanCheck
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv()
     qrraw = 't=20230306T1324&s=59.98&fn=7281440500232639&i=8355&fp=3680124697&n=1'
     client = ScanCheck()
     check_json = client.get_json(qrraw)
