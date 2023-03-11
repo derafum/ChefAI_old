79a9eaf587c73af216292d814ff6437983dd8b0e
George <krasavingeorg@gmail.com>
2023-03-11 16:56:28 +0400

    Перенес получение токена в основной файл

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
+
 from get_check import ScanCheck
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     qrraw = 't=20230306T1341&s=128.97&fn=9960440302156797&i=74284&fp=1765630299&n=1'
-    client = ScanCheck()
+    client = ScanCheck(os.getenv('TOKEN'))
     check_json = client.get_json(qrraw)
     client.save_check_to_json()
