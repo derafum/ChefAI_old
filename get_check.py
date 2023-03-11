771a7c8a1cbda2aca6f8984e820be4d955356ccc
George <krasavingeorg@gmail.com>
2023-03-11 04:51:12 +0400

    Добавил docstring к классу ScanCheck и его методам. Изменил метод сохранения json файла save_check_to_json

--- get_check.py
+++ get_check.py
@@ -1,23 +1,47 @@
+import requests
+import json
 import os
 
-import json
-import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class ScanCheck:
+    """
+        Класс для работы с чеком.
+        ...
+        Атрибуты
+        --------
+        url : str
+            ссылка на proverkacheka
+        token : str
+            персональный токен
+        last_responce : None
+            последний полученный чек
+        Методы
+        ------
+        get_json(qrraw: str) -> dict:
+            Получаем чек в формате json
+
+        save_check_to_json(filename:str = "info_check.json"):
+            Сохраняем данные чека в формате json
+        """
     def __init__(self):
         self.url = 'https://proverkacheka.com/api/v1/check/get'
         self.token = os.getenv('TOKEN')
         self.last_response = None
 
-    def get_json(self, qrraw: str):
-        """Получить результат проверки чека в json формате"""
+    def get_json(self, qrraw: str) -> dict:
+        """Получаем данные чека и переводим в json"""
         data = {'token': self.token,
                 'qrraw': qrraw}
-        self.last_response = requests.post(self.url, data=data)
-        return json.loads(self.last_response.text)
+        resp = requests.post(self.url, data=data)
+        file_json = json.loads(resp.text)
+        self.last_response = file_json
+        return file_json
 
-    def save_check_to_json(self, filename: str = 'info_check.json'):
-        """Сохраняем данные чека в виде json"""
-        with open(filename, mode='w', encoding='utf-8') as file:
-            json.dump(json.loads(self.last_response.text), file)
+    def save_check_to_json(self, filename:str = "info_check.json"):
+        """Сохраняем данные чека в формате json"""
+        with open(filename, 'w') as f:
+            json.dump(self.last_response, f, ensure_ascii=False, indent=2)
