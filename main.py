35457eb2753a60af86056824f55464027f1e8d90
George <krasavingeorg@gmail.com>
2023-03-11 04:51:12 +0400

    Изменил работу с методом save_check_to_json

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from get_check import ScanCheck
-from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    load_dotenv()
-    qrraw = 't=20230306T1324&s=59.98&fn=7281440500232639&i=8355&fp=3680124697&n=1'
+    qrraw = 't=20230306T1341&s=128.97&fn=9960440302156797&i=74284&fp=1765630299&n=1'
     client = ScanCheck()
     check_json = client.get_json(qrraw)
     client.save_check_to_json()
+
